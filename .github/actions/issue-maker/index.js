b360295b816e4396100b406a13a8e209bc393d10
Orine7 <eduardofilho_08@hotmail.com>
2022-02-08 17:23:11 -0300

    add instance creation

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,7 +7,7 @@ async function run() {
         const jokeBody = core.getInput("joke");
         const token = core.getInput("repo-token");
 
-        const octokit = github.getOctokit(token);
+        const octokit = new github.getOctokit(token);
 
         const newIssue = await octokit.issues.create({
             repo: github.context.repo.repo,
@@ -15,6 +15,7 @@ async function run() {
             title: issueTitle,
             body: jokeBody
         })
+        return newIssue
     } catch (err) {
         core.setFailed(err.message)
     }
